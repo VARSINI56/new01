45bdf1c50c1edf9778496db34f6156338bf509f9
VARSINI56 <varsinik21@gmail.com>
2026-04-01 11:43:06 +0530

    Update f2.py

--- f2.py
+++ f2.py
@@ -3,12 +3,3 @@
 # Print a greeting
 print("Hello, World!")
 
-# Get input from the user
-a = int(input("Enter first number: "))
-b = int(input("Enter second number: "))
-
-# Calculate sum
-sum_result = a + b
-
-# Print the result
-print(f"Sum of {a} and {b} is: {sum_result}")

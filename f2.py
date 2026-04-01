5addde049bb56b853eb8815b1a5e27e9a0457403
Varsini <varsinik21@gmail.com>
2026-04-01 11:06:28 +0530

    commit 4

---
+++ f2.py
@@ -0,0 +1,14 @@
+// Sample Python Program: Hello and Addition
+
+# Print a greeting
+print("Hello, World!")
+
+# Get input from the user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Calculate sum
+sum_result = a + b
+
+# Print the result
+print(f"Sum of {a} and {b} is: {sum_result}")

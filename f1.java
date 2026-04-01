7edcd10ee28ae6702cce51df1271d8530368e4e2
Varsini <varsinik21@gmail.com>
2026-04-01 10:49:28 +0530

    commit 1

---
+++ f1.java
@@ -0,0 +1,17 @@
+// Basic Java Program: Hello World and Simple Addition
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // Print a message
+        System.out.println("Hello, World!");
+
+        // Simple addition
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+
+        // Print the result
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
